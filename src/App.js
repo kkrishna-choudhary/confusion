b9c12ceb1023675ea8fd651bda4c18e54deb5698
Krishna Kumar Choudhary <kc89848@gmail.com>
2021-04-08 01:21:49 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import {Component} from 'react';
 import './App.css';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
  
@@ -9,9 +10,11 @@ class App extends Component {
   render() {
 
     return (
-      <div >
+      <BrowserRouter>
+      <div className="App">
         <Main />
       </div>
+    </BrowserRouter>
     );
 
   }
