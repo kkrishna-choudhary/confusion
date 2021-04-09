89334896d81bed5724be35bbdad8cdd6c70d34de
Krishna Kumar Choudhary <kc89848@gmail.com>
2021-04-09 23:28:55 +0530

    Assignment2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import {Component} from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetails from './DishDetails';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -51,6 +52,7 @@ class Main extends Component {
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders}/>} />
               <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
           </Switch>
